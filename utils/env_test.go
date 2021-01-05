512ec813b64a717fb7c99f18758d42d4e894934a
Keng Suppaseth <kengio@gmail.com>
2021-01-05 14:41:33 +0700

    WIP: Add tests for utils

--- utils/env_test.go
+++ utils/env_test.go
@@ -1,13 +1,30 @@
 package utils
 
 import (
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetEnv(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-		GetEnv("", "")
-		t.Error("Missing test case")
+	assert := assert.New(t)
+
+	t.Run("no env with default value should return default value", func(t *testing.T) {
+		myEnv := GetEnv("MY_ENV", "Default Value")
+		assert.Equal(myEnv, "Default Value")
+	})
+
+	t.Run("have env and default value should return env", func(t *testing.T) {
+		os.Setenv("MY_ENV", "Test")
+		myEnv := GetEnv("MY_ENV", "Default Value")
+		assert.Equal(myEnv, "Test")
+	})
+
+	t.Run("empty env and default value should return empty", func(t *testing.T) {
+		os.Setenv("MY_ENV", "")
+		myEnv := GetEnv("MY_ENV", "")
+		assert.Empty(myEnv)
 	})
 }
 
--- utils/go.mod
+++ utils/go.mod
@@ -1,3 +1,5 @@
 module github.com/openware/pkg/utils
 
+require github.com/stretchr/testify v1.6.1
+
 go 1.15
