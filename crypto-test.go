f9329e3ef6127833ea88f95d835cf33fe7720c00
Danylo Patsora <dpatsora@heliostech.fr>
2021-01-25 03:32:30 +0200

    Add files to test

---
+++ crypto-test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/openware/pkg/cryptocom"
+)
+
+func main() {
+	client := cryptocom.New("test", "test")
+
+	err := client.Connect("wss://uat-stream.3ona.co/v2/market")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client.SubscribeTrades("ETH_BTC")
+
+	msgs := client.Listen()
+
+	for m := range msgs {
+		fmt.Println(m)
+	}
+
+	// r := client.AuthRequest()
+	// b, _ := r.Encode()
+
+	// var res interface{}
+	// err := json.Unmarshal(b, &res)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// m := res.(map[string]interface{})
+
+	// fmt.Println(m)
+}
