08a80973e20591ea57ca656c6d3f8cbaaa001c1a
MoD <mod@tail.cat>
2021-04-28 01:17:22 +0300

    Adding useful package for Finex and TradePoint

---
+++ currency/forexprovider/exchangeratesapi.io/exchangeratesapi_types.go
@@ -0,0 +1,44 @@
+package exchangerates
+
+import (
+	"time"
+
+	"github.com/openware/pkg/currency/forexprovider/base"
+	"github.com/openware/pkg/request"
+)
+
+const (
+	exchangeRatesAPI                 = "https://api.exchangeratesapi.io"
+	exchangeRatesLatest              = "latest"
+	exchangeRatesHistory             = "history"
+	exchangeRatesSupportedCurrencies = "EUR,CHF,USD,BRL,ISK,PHP,KRW,BGN,MXN," +
+		"RON,CAD,SGD,NZD,THB,HKD,JPY,NOK,HRK,ILS,GBP,DKK,HUF,MYR,RUB,TRY,IDR," +
+		"ZAR,INR,AUD,CZK,SEK,CNY,PLN"
+
+	rateLimitInterval = time.Second * 10
+	requestRate       = 10
+)
+
+// ExchangeRates stores the struct for the ExchangeRatesAPI API
+type ExchangeRates struct {
+	base.Base
+	Requester *request.Requester
+}
+
+// Rates holds the latest forex rates info
+type Rates struct {
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+	Rates map[string]float64 `json:"rates"`
+}
+
+// HistoricalRates stores the historical rate info
+type HistoricalRates Rates
+
+// TimeSeriesRates stores time series rate info
+type TimeSeriesRates struct {
+	Base    string                 `json:"base"`
+	StartAt string                 `json:"start_at"`
+	EndAt   string                 `json:"end_at"`
+	Rates   map[string]interface{} `json:"rates"`
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/openware/pkg
+
+go 1.16
+
+require golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba
