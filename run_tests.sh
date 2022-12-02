1fe97f8131dbc2b821e4a4da5779c9c7d83ad270
Vee <vjirapon@openware.com>
2022-12-02 14:48:15 +0700

    feat(ci): integrate go-coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 set -xe
 
+if [ -z ${COVER_OUT} ]; then
+  echo "COVER_OUT is required"
+  exit 1
+fi
+
 ROOT_DIR=$PWD
 
 for d in */ ; do
 	cd $d
 
 	go mod download
-	go test ./... -cover
+	go test ./... --cover -coverprofile $COVER_OUT
 	go vet ./...
 
 	cd $ROOT_DIR
